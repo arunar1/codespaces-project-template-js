da0f00d3de3352a9e327273484a5adc7ffa921e3
Arun A R <121934657+arunar1@users.noreply.github.com>
2024-06-15 07:48:17 +0000

    added the skillset(#2)

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Sciecne Engineering student studying at RIT. I enjoy creating user-friendly websites and applications aim to introduce innovative solutions that improve people’s lives.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "Reactjs",
+  "Nodejs",
+  "Javascript",
+  "React-Native",
   "Mobile user interfaces",
   "Graphic design",
+  "Problem solving"
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging web development experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
